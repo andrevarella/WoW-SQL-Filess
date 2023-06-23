95a31aa8f07759dafc57b01cb14bb101f3cbbba2
andrevarella <andre.varella@hotmail.com>
2023-06-23 15:17:08 -0300

    revert npc add reputaçao

--- Lua Scripts Server/NPC_AddReputaçaoICC.lua
+++ Lua Scripts Server/NPC_AddReputaçaoICC.lua
@@ -2,9 +2,12 @@ local unitEntry = 94125
 
 local function OnGossipHello(event, player, object)
     player:GossipClearMenu()
-    player:GossipMenuAddItem(0, "Exalted em Ashen Verdict", 0, 1)
+    player:GossipMenuAddItem(0, "Honored em Ashen Verdict", 0, 1)
     player:GossipSendMenu(1, object)
 end
+
+-- Sua reputação atual com Ashen Verdict é acima de Honored.
+-- Você já é honored com Ashen Verdict.
 
 -- 3000 = Friendly
 -- 9000 = Honored
@@ -13,10 +16,19 @@ end
 
 local function OnGossipSelect(event, player, object, sender, intid, code)
     if (intid == 1) then
-        player:SetReputation(1156, 9000)
-        player:GossipComplete()
+        local currentReputation = player:GetReputation(1156)
+        local reputationToAdd = 9000
+
+        if currentReputation < reputationToAdd then
+            player:SetReputation(1156, reputationToAdd)
+            player:GossipComplete()
+        else
+            player:SendBroadcastMessage("Você já é honored com Ashen Verdict.")
+        end
     end
 end
 
 RegisterCreatureGossipEvent(unitEntry, 1, OnGossipHello)
 RegisterCreatureGossipEvent(unitEntry, 2, OnGossipSelect)
+
+
